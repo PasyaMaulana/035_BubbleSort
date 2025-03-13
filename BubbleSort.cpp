5dd53e0fbd2e67e3fa03320ec5de8450220cf13b
PasyaMaulana <pasyamaulana17@gmail.com>
2025-03-13 11:41:11 +0700

    prosedur bubblesortarray

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -30,3 +30,23 @@ void input() //prosedur untuk input
     }
 }
 
+void bubbleSortArray(){
+    int pass = 1; //step 1
+
+    do
+    {//step 2
+        for (int j = 0; j <= n -1-pass;j++) //step 3
+    {
+        if (arr[j] > arr[j+1]){
+            int temp;
+            temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+        }
+
+        pass = pass + 1; //step 4
+    }
+        
+
+    }while (pass <= n - 1); //step 5
+}
